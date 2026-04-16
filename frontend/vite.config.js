61c11163e94ed3a97760872cffd750f964e1e3d0
Maxim <makc0791@gmail.com>
2026-04-16 22:41:17 +0300

    fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Дозволяємо хост Rahti
+    allowedHosts: [
+      'sentiment-analysis-ai-sentiment-analysis2.2.rahtiapp.fi'
+    ],
+    // Також важливо переконатися, що порт збігається
+    port: 8080,
+    host: true
+  }
 })
